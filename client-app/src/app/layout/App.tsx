68180307d781583e29647848c9d5e199997a6c1c
maks27 <maks2796@gmail.com>
2021-01-04 21:38:44 +0100

    front changes

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([])
+  useEffect(()=>{
+    axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response)=>{
+      setActivities(response.data)
+     })
+
+  },[])
+  
+  return (
+    <div>
+      <NavBar/>
+      <List>
+        {activities.map((activity) => {
+          return <List.Item key={activity.id}>{activity.title}</List.Item>;
+        })}
+      </List>
+    </div>
+  );
+};
+
+export default App;
---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+const NavBar = () => {
+  return (
+    <Menu fixed='top' inverted>
+      <Container>
+      <Menu.Item header>
+          <img src="/assets/logo.png" alt="logo"/>
+          Reactivitis
+      </Menu.Item>
+      <Menu.Item name="Activities" />
+      <Menu.Item>
+          <Button positive content="Create Activity"></Button>
+      </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+export default NavBar
